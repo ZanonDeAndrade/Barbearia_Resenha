70d9fe27e126921397938a9b0027c8a3bd4482eb
Arthur Zanon <zanondea@gmail.com>
2024-04-29 15:58:26 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+// script.js
+
+// Função para redirecionar para a página de Bruno
+function redirecionarParaBruno() {
+    window.location.href = 'bruno.html'; // Substitua 'pagina_bruno.html' pelo URL da página de Bruno
+  }
+  
+  // Função para redirecionar para a página de Natan
+  function redirecionarParaNatan() {
+    window.location.href = 'natan.html'; // Substitua 'pagina_natan.html' pelo URL da página de Natan
+  }
+  
+  function redirecionarParaInicio(){
+    window.location.href = 'index.html'
+  }
+
+  function redirecionarParaCorteCabelo_Bruno(){
+    window.location.href = 'Corte_cabelo_Bruno.html'
+  }
+
+  function redirecionarParaCorteMaquina_Bruno(){
+    window.location.href = 'Corte_maquina_Bruno.html'
+  }
+
+  function redirecionarParaCorteBarba_Bruno(){
+    window.location.href = 'Barba_completa_Bruno.html'
+  }
+
+  function redirecionarParaCorteCabelo_e_Barba_Bruno(){
+    window.location.href = 'Barba_e_cabelo_Bruno.html'
+  }
+
+  function redirecionarParaCorteCabeloCrianca_Bruno(){
+    window.location.href = 'Corte_crianca_Bruno.html'
+  }
+
+
+
+
+
+  function redirecionarParaCorteCabelo_Natan(){
+    window.location.href = 'Corte_cabelo_Natan.html'
+  }
+
+  function redirecionarParaCorteMaquina_Natan(){
+    window.location.href = 'Corte_maquina_Natan.html'
+  }
+
+  function redirecionarParaCorteBarba_Natan(){
+    window.location.href = 'Barba_completa_Natan.html'
+  }
+
+
+  function redirecionarParaCorteCabelo_e_Barba_Natan(){
+    window.location.href = 'Barba_e_cabelo_Natan.html'
+  }
+
+
+  function redirecionarParaCorteCabeloCrianca_Natan(){
+    window.location.href = 'Corte_crianca_Natan.html'
+  }
+
+
+  
